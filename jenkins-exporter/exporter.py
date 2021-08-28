e360582a2035c6fb7a84e590ed24fdf75a804517
roman <warrior7089@gmail.com>
2021-08-28 14:20:41 +0300

    get jenkins jobs count metric

--- jenkins-exporter/exporter.py
+++ jenkins-exporter/exporter.py
@@ -1,11 +1,15 @@
 import time
 import random
 
-from prometheus_client import start_http_server
+from prometheus_client import start_http_server, Gauge
+from api4jenkins import Jenkins
 
+jenkins_client = Jenkins('http://jenkins:8080/', auth=('admin', 'admin'))
+
+JENKINS_JOB_COUNT = Gauge('jenkins_jobs_count', "Number of Jenkins jobs")
 
 def get_metrics():
-    pass
+    JENKINS_JOB_COUNT.set(len(list(jenkins_client.iter_jobs())))
 
 
 if __name__ == '__main__':
